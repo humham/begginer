87ba5b59aba64d6cfb7f719483e99bace60e6c09
humham <107066381+humham@users.noreply.github.com>
2022-06-07 18:58:15 +0200

    Add files via upload

---
+++ rules.py
@@ -0,0 +1,35 @@
+#
+#Forbidden python features
+#    imports,
+#    from imports,
+#    defining functions with builtin names,
+#    calls of function with builtin names except ['int', 'list', 'dict', 'bool', 'str', 'float', 'tuple', 'None', 'type', 'ord', 'chr'] ,
+#    calls of methods (eg. 'my_list.append()'),
+#    try clauses,
+#    break and continue statements,
+#    variables with builtin names or trailing underscore (eg. 'my_var_'),
+#    for loop,
+#    list comprehensions,
+#    dict comprehensions,
+#    set comprehensions,
+#    generator expressions,
+#    yield and yield from statements,
+#    raise statements,
+#    assert statements,
+#    while else statements,
+#    global statements,
+#    nonlocal statements,
+#    following operators on list: ['+', '+=', '==', '>=', '<=', '>', '<', '!=', '*='],
+#    3 args form of type,
+#    class definitions,
+#    in and not in operator,
+#    is and is not operator,
+#    positional only arguments, keyword only arguments, keywords defaults arguments, defaults arguments, variadic arguments, variadic keyword arguments,
+#    slices,
+
+#You must respect two other rules:
+
+#    Line length must be less than 80 characters,
+#    Function length must be less than 25 lines.
+
+print("rules")
